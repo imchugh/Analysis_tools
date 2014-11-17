ed3c75d079c84638b23dbd59400e204230613d73
imchugh <ian.mchugh@monash.edu>
2014-11-17 16:44:44 +1100

    Just most recent versions of these

--- DataIO.py
+++ DataIO.py
@@ -12,7 +12,7 @@ import datetime as dt
 import xlrd
 import pdb
 
-def netCDF_input():
+def OzFluxQCnc_to_pandasDF():
     
     # Prompt user for configuration file and get it
     root = Tkinter.Tk(); root.withdraw()
@@ -36,3 +36,46 @@ def netCDF_input():
     df=pd.DataFrame(d_data,index=dates_list)    
     
     return df, d_attr
+
+# Multiple sheets are returned as dictionary (pandas dataframe) objects
+def xlsx_to_pandas(header=True,header_row=0,skiprows_after_header=0,date_col=True):
+    
+    # Prompt user for configuration file and get it
+    root = Tkinter.Tk(); root.withdraw()
+    file_in = tkFileDialog.askopenfilename(initialdir='')
+    root.destroy()
+
+    xl_book=xlrd.open_workbook(file_in)
+    
+    d={}
+    
+    start_date='1900-01-01'
+    end_date='2100-01-01'    
+    
+    for sheet_name in xl_book.sheet_names():
+
+        sheet=xl_book.sheet_by_name(sheet_name)
+
+        rows=sheet.nrows
+        cols=sheet.ncols
+        
+        if header==True:
+            if date_col==True:
+                column_names=[str(sheet.cell_value(header_row,i)) for i in range(cols)]
+                index=[]
+                for i in xrange(header_row+skiprows_after_header+1,sheet.nrows):
+                    try:
+                        index.append(dt.datetime(*xlrd.xldate_as_tuple(sheet.cell_value(i,0), xl_book.datemode)))
+                    except ValueError:
+                        index.append('')
+                        print 'Error in sheet '+sheet_name+' at row '+str(i)+'; missing or invalid datetime stamp! Skipping...'
+                df=pd.DataFrame(columns=column_names[1:],index=index)
+                for i in range(1,cols):
+                    df[column_names[i]]=[sheet.cell_value(j,i) for j in xrange(header_row+skiprows_after_header+1,sheet.nrows)]
+                df_ind=pd.date_range(start=df.index[0],end=df.index[-1],freq='30Min')
+                df=df.reindex(df_ind)
+                d[sheet_name]=df
+                
+    return d
+
+        
--- plots_generic.py
+++ plots_generic.py
@@ -2,19 +2,121 @@
 """
 Created on Thu Aug 21 17:00:07 2014
 
+Pass dictionary containing the following parameters:
+    - 'xlab' - xlabel
+    - 'ylab' - ylabel
+    - 'title'
+
 @author: imchugh
 """
 
 import matplotlib.pyplot as plt
+import pdb
 
-def line_plot(df,title):
+def line_plot(df,d):
     fig=plt.figure(figsize=(12,8))
     fig.patch.set_facecolor('white')
-    for i in df.columns:
-        plt.plot(df.index,df[i],label=i)
-    plt.title(title)
-    plt.xlabel('Day of year',fontsize=16)
-    plt.ylabel('Cumulative daily NEE $(gC\/m^{-2})$',fontsize=16)
-    plt.axvline(y=0,color='black',linestyle='-')
-    plt.legend(loc='upper right')
+    for i,var in enumerate(df.columns):
+        if 'colors' in d:
+            plt.plot(df.index,df[var],label=var,color=d['colors'][i])
+        else:
+            plt.plot(df.index,df[var],label=var,linewidth=1)
+    plt.title(d['title'],fontsize=24)
+    plt.xlabel(d['xlab'],fontsize=18)
+    plt.ylabel(d['ylab'],fontsize=18)
+    plt.tick_params(labelsize=14)
+    plt.xticks(rotation=45)
+    if 'vert_line' in d:
+        plt.axvline(x=d['vert_line'],color='black',linestyle='--')
+    if 'hor_line' in d:
+        plt.axhline(y=d['hor_line'],color='black',linestyle='-')
+    plt.legend(loc='upper left')
     plt.show()
+
+def one_ax_dict_set():
+    """
+    Returns dictionary required to run the two y axis algorithm above
+    """
+    d={}
+
+    d['title']='Dendrometer stem increment\n'
+    d['xlab']='\nDate'
+    d['ylab']='Increment (mm)'
+    d['colors']=['green','red','blue','purple','orange','brown','yellow']
+    #d['vert_line']=False
+    #d['hor_line']=False
+    
+    return d
+
+def fill_line_plot(df,d):
+    fig=plt.figure(figsize=(12,8))
+    fig.patch.set_facecolor('white')
+    x=df.index    
+    y1=df[df.columns[0]]
+    y2=df[df.columns[1]]
+    plt.xlim((0,24))
+    plt.plot(x,y1,color='black',linestyle='-.',linewidth=2,label=df.columns[0])
+    plt.plot(x,y2,color='black',linestyle='-',linewidth=2,label=df.columns[1])
+    plt.fill_between(x, y1, y2, where=y2>=y1, facecolor='blue', edgecolor='None',interpolate=True)
+    plt.fill_between(x, y1, y2, where=y1>=y2, facecolor='red', edgecolor='None',interpolate=True)
+    plt.xlabel(d['xlab'],fontsize=18)
+    plt.ylabel(d['ylab'],fontsize=18)
+    plt.title(d['title'],fontsize=24)
+    plt.xticks([0,4,8,12,16,20,24])
+    if 'vert_line' in d:
+        plt.axvline(x=d['vert_line'],color='black',linestyle='-')
+    if 'hor_line' in d:
+        plt.axhline(y=d['hor_line'],color='black',linestyle='-')
+    plt.legend(loc='lower right')
+    
+def two_ax_line_plot(df,d):
+    """
+    Pass dataframe (df) and dictionary (d) specified by function 
+    'two_ax_dict_set' - see below;
+    returns open plot
+    """
+    fig=plt.figure(figsize=(12,8))
+    fig.patch.set_facecolor('white')
+    ax = plt.gca()
+    ax2 = ax.twinx()
+    y1=d['y1']
+    y2=d['y2']
+    for i,var in enumerate(y1['vars']):
+        if 'colors' in y1:
+            ax.plot(df.index,df[var],label=var,color=y1['colors'][i],linewidth=1)
+        else:
+            ax.plot(df.index,df[var],label=var,linewidth=1)
+    for i,var in enumerate(y2['vars']):
+        if 'colors' in y2:
+            ax2.plot(df.index,df[var],label=var,color=y2['colors'][i],linewidth=1)
+    plt.title(d['globals']['title'],fontsize=24)
+    ax.set_xlabel(d['globals']['xlab'],fontsize=18)
+    ax.set_ylabel(y1['lab'],fontsize=18)
+    ax2.set_ylabel(y2['lab'],fontsize=18)
+    plt.tick_params(labelsize=14)
+    ax.legend(loc='upper left',fontsize=14)
+    ax2.legend(loc='upper right',fontsize=14)
+    plt.show()
+
+def two_ax_dict_set():
+    """
+    Returns dictionary required to run the two y axis algorithm above
+    """
+    d={}
+
+    d['globals']={}
+    d['globals']['title']='Fc and ET\n'
+    d['globals']['xlab']='\nDate'
+
+    d['y1']={}
+    d['y1']['vars']=['GPP_SOLO_rm']
+    d['y1']['colors']=['green']
+    d['y1']['lab']='$GPP\/(gC\/m^{2}d^{-1})$'
+
+    d['y2']={}
+    d['y2']['vars']=['Sws']
+    d['y2']['colors']=['blue']
+    d['y2']['lab']='$Sws\/(m^{3}m^{-3})$'
+        
+    return d
+    
