ae86c02d0e29177df3aaa826e8b3edbbc69628a3
imchugh <ian.mchugh@monash.edu>
2014-11-26 16:53:35 +1100

    Rewrote excel import to allow reindexing of variable frequencies; also added a similar dialog file selection script

--- DataIO.py
+++ DataIO.py
@@ -12,12 +12,15 @@ import datetime as dt
 import xlrd
 import pdb
 
-def OzFluxQCnc_to_pandasDF():
+# Prompt user for configuration file and get it, then return file path
+def file_select_dialog():
     
-    # Prompt user for configuration file and get it
     root = Tkinter.Tk(); root.withdraw()
     file_in = tkFileDialog.askopenfilename(initialdir='')
     root.destroy()
+    return file_in
+
+def OzFluxQCnc_to_pandasDF(file_in):
     
     nc_obj=netCDF4.Dataset(file_in)
     
@@ -38,12 +41,15 @@ def OzFluxQCnc_to_pandasDF():
     return df, d_attr
 
 # Multiple sheets are returned as dictionary (pandas dataframe) objects
-def xlsx_to_pandas(header=True,header_row=0,skiprows_after_header=0,date_col=True):
-    
-    # Prompt user for configuration file and get it
-    root = Tkinter.Tk(); root.withdraw()
-    file_in = tkFileDialog.askopenfilename(initialdir='')
-    root.destroy()
+# Note XLRD cell type codes:
+#    XL_CELL_EMPTY: 0
+#    XL_CELL_TEXT: 1 (STRING)
+#    XL_CELL_NUMBER: 2 (FLOAT)
+#    XL_CELL_DATE: 3 (FLOAT)
+#    XL_CELL_BOOLEAN: 4 (INT)
+#    XL_CELL_ERROR: 5 (INTERNAL EXCEL CODE)
+#    XL_CELL_BLANK: 6 (EMPTY STRING)
+def xlsx_to_pandas(file_in,header=True,header_row=0,skiprows_after_header=0,date_col=True,reindex=True):
 
     xl_book=xlrd.open_workbook(file_in)
     
@@ -55,27 +61,36 @@ def xlsx_to_pandas(header=True,header_row=0,skiprows_after_header=0,date_col=Tru
     for sheet_name in xl_book.sheet_names():
 
         sheet=xl_book.sheet_by_name(sheet_name)
-
+        
+        print sheet_name        
+        
         rows=sheet.nrows
         cols=sheet.ncols
         
-        if header==True:
-            if date_col==True:
-                column_names=[str(sheet.cell_value(header_row,i)) for i in range(cols)]
-                index=[]
-                for i in xrange(header_row+skiprows_after_header+1,sheet.nrows):
-                    try:
-                        index.append(dt.datetime(*xlrd.xldate_as_tuple(sheet.cell_value(i,0), xl_book.datemode)))
-                    except ValueError:
-                        index.append('')
-                        print 'Error in sheet '+sheet_name+' at row '+str(i)+'; missing or invalid datetime stamp! Skipping...'
-                df=pd.DataFrame(columns=column_names[1:],index=index)
-                for i in range(1,cols):
-                    df[column_names[i]]=[sheet.cell_value(j,i) for j in xrange(header_row+skiprows_after_header+1,sheet.nrows)]
-                df_ind=pd.date_range(start=df.index[0],end=df.index[-1],freq='30Min')
-                df=df.reindex(df_ind)
-                d[sheet_name]=df
-                
+        if rows==0: print 'Could not find any valid rows'
+        if cols==0: print 'Could not find any valid columns'
+        
+        if rows!=0 and cols!=0:
+            if header==True:
+                if date_col==True:
+                    column_names=[str(sheet.cell_value(header_row,i)) for i in range(cols)]
+                    index=[]
+                    for i in xrange(header_row+skiprows_after_header+1,sheet.nrows):
+                        try:
+                            index.append(dt.datetime(*xlrd.xldate_as_tuple(sheet.cell_value(i,0), xl_book.datemode)))
+                        except ValueError:
+                            index.append('')
+                            print 'Error in sheet '+sheet_name+' at row '+str(i)+'; missing or invalid datetime stamp! Skipping...'
+                    df=pd.DataFrame(columns=column_names[1:],index=index)
+                    for i in range(1,cols):
+                        df[column_names[i]]=sheet.col_values(i)[header_row+skiprows_after_header+1:]
+                    if reindex==True:
+                        df_freq=pd.infer_freq(df.index)
+                        df_ind=pd.date_range(start=df.index[0],end=df.index[-1],freq=df_freq)
+                        df=df.reindex(df_ind)
+                    d[sheet_name]=df
+        else:
+            d[sheet_name]=pd.DataFrame()
     return d
 
         
