9b4fcc28f2aaea85bebdc0d737156cafbdf663ca
imchugh <ian.mchugh@monash.edu>
2014-11-26 16:54:12 +1100

    Added stacked bar plots

--- plots_generic.py
+++ plots_generic.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 import pdb
 
 def line_plot(df,d):
-    fig=plt.figure(figsize=(12,8))
+    fig=plt.figure(figsize=(16,8))
     fig.patch.set_facecolor('white')
     for i,var in enumerate(df.columns):
         if 'colors' in d:
@@ -27,10 +27,12 @@ def line_plot(df,d):
     plt.tick_params(labelsize=14)
     plt.xticks(rotation=45)
     if 'vert_line' in d:
-        plt.axvline(x=d['vert_line'],color='black',linestyle='--')
+        for i in d['vert_line']:
+            plt.axvline(x=i,color='black',linestyle='dotted',linewidth=0.5)
     if 'hor_line' in d:
-        plt.axhline(y=d['hor_line'],color='black',linestyle='-')
-    plt.legend(loc='upper left')
+        for i in d['hor_line']:
+            plt.axhline(y=i,color='black',linestyle='-')
+    plt.legend(loc='lower left')
     plt.show()
 
 def one_ax_dict_set():
@@ -39,12 +41,12 @@ def one_ax_dict_set():
     """
     d={}
 
-    d['title']='Dendrometer stem increment\n'
+    d['title']='Whroo vs Riggs NEE\n'
     d['xlab']='\nDate'
-    d['ylab']='Increment (mm)'
-    d['colors']=['green','red','blue','purple','orange','brown','yellow']
-    #d['vert_line']=False
-    #d['hor_line']=False
+    d['ylab']='$NEE\/(gC\/m^{-2}d^{-1})$'
+    d['colors']=['blue','red']
+    d['vert_line']=['2012-04-01','2012-07-01','2012-10-01','2013-01-01','2013-04-01','2013-07-01','2013-10-01']
+    d['hor_line']=[0]
     
     return d
 
@@ -89,6 +91,12 @@ def two_ax_line_plot(df,d):
     for i,var in enumerate(y2['vars']):
         if 'colors' in y2:
             ax2.plot(df.index,df[var],label=var,color=y2['colors'][i],linewidth=1)
+    if 'vert_line' in d['globals']:
+        for i in d['globals']['vert_line']:
+            plt.axvline(x=i,color='black',linestyle='dotted',linewidth=0.5)
+    if 'hor_line' in d['globals']:
+        for i in d['globals']['hor_line']:
+            plt.axhline(y=i,color='black',linestyle='-')        
     plt.title(d['globals']['title'],fontsize=24)
     ax.set_xlabel(d['globals']['xlab'],fontsize=18)
     ax.set_ylabel(y1['lab'],fontsize=18)
@@ -105,18 +113,59 @@ def two_ax_dict_set():
     d={}
 
     d['globals']={}
-    d['globals']['title']='Fc and ET\n'
+    d['globals']['title']='GPP and ET Riggs\n'
     d['globals']['xlab']='\nDate'
+    d['globals']['vert_line']=['2012-04-01','2012-07-01','2012-10-01','2013-01-01','2013-04-01','2013-07-01','2013-10-01']
 
     d['y1']={}
-    d['y1']['vars']=['GPP_SOLO_rm']
+    d['y1']['vars']=['r_GPP_rm']
     d['y1']['colors']=['green']
     d['y1']['lab']='$GPP\/(gC\/m^{2}d^{-1})$'
 
     d['y2']={}
-    d['y2']['vars']=['Sws']
+    d['y2']['vars']=['r_ET_rm']
     d['y2']['colors']=['blue']
-    d['y2']['lab']='$Sws\/(m^{3}m^{-3})$'
+    d['y2']['lab']='$ET\/(gH_{2}O\/m^{-2}d^{-1})$'
         
     return d
+
+def stacked_bar_plot(df,d):
+    """    
+    Pass dataframe (df) and dictionary (d) specified by function 
+    'stacked_bar_dict_set' - see below;
+    returns open plot
+    """
+    fig=plt.figure(figsize=(12,8))
+    fig.patch.set_facecolor('white')
+    ax = plt.gca()
+    width=0.35
+    plt.xlim(0.85,1.5)
+    sum_vars=[]
+    for i,var in enumerate(df.columns):
+        sum_vars==[] if i==0 else sum_vars.append(df.ix[0,i-1])
+        if 'colors' in d:
+            plt.bar(df.index+1, df[var], width, color=d['colors'][i], label=var,bottom=sum(sum_vars))
+    ax.axes.get_xaxis().set_ticks([])        
+    plt.title(d['title'],fontsize=24)
+    plt.ylabel(d['ylab'],fontsize=18)
+    if 'xlab' in d: plt.ylabel(d['xlab'],fontsize=18)
+    if 'ylab' in d: plt.ylabel(d['ylab'],fontsize=18)    
+#    plt.legend(loc='upper right')
+    plt.text(1.175,36,'Aboveground Vegetation:\n 37.75', fontsize=18,horizontalalignment='center',verticalalignment='center')
+    plt.text(1.175,15,'Litter: 5.8', fontsize=18,horizontalalignment='center',verticalalignment='center')
+    plt.text(1.175,5,'Belowground vegetation:\n 10.74', fontsize=18,horizontalalignment='center',verticalalignment='center')
+    bbox_props = dict(boxstyle="larrow,pad=0.3", ec="black", fc='white', lw=2)
+    plt.text(1.38,11,'Soil: 1.69',fontsize=18,bbox=bbox_props)
+    fig.show()
     
+def stacked_bar_dict_set():
+    """
+    Returns dictionary required to run the two y axis algorithm above
+    """
+    d={}
+    
+    d['title']='Whroo carbon pools\n'
+    d['colors']=['blue','brown','grey','green']
+    d['ylab']='$C\/storage\/(tC\/ha^{-1})$'
+    
+    return d
