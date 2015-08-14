69bc6b4a4b727686d78d9b478efc3d80218a4fb5
imchugh <ian.mchugh@monash.edu>
2015-08-14 16:56:57 +1000

    Update

--- uncertainty_wrapper_OzFluxQC.py
+++ uncertainty_wrapper_OzFluxQC.py
@@ -12,39 +12,36 @@ import datetime as dt
 import netCDF4
 import xlrd
 
-import random_error_2 as r_e
+import random_error_3 as r_e
 
 # Open configuration and build dictionaries of config file contents
 def get_configs():
     
-    configs_dict = {'flux_period': 30,
-                    'averaging_bins': 100,
-                    'radiation_difference_threshold': 10,
+    configs_dict = {'measurement_interval': 30,
+                    'neg_averaging_bins': 10,
+                    'pos_averaging_bins': 10,
+                    'radiation_difference_threshold': 35,
                     'temperature_difference_threshold': 3,
                     'windspeed_difference_threshold': 1,
                     'noct_threshold': 5,
-                    'propagate_error': False,
-                    'num_trials': 10**4,
                     'ustar_threshold': 0,
-                    'error_generator': False,
+                    'num_trials': 10**4,
                     'nan_value': -9999,
                     'QC_accept_code': 0,
-                    'propagation_series': 'Fc',
-                    'error_generation_series': 'Fc',
-                    'model_series': ''}
+                    'mean_flux_series': 'Fc_model'}
                     
     return configs_dict
 
 def get_data(configs_dict):
 
     # Specify file location and name
-    data_input_path = 'E:\\' 
-    # '/home/imchugh/Ozflux/Sites/Whroo/Data/Processed/all'
-    data_input_file = 'Whroo_2011_to_2014_L6.nc'
+    data_input_path = '/home/imchugh/Ozflux/Sites/Whroo/Data/Processed/all'
+    data_input_file = 'Whroo_2011_to_2014_L5.nc'
     data_input_target = os.path.join(data_input_path, data_input_file)
 
     # Initialise dictionaries
     vars_dict = {'carbon_flux':'Fc',
+                 'modelled_carbon_flux': 'Fc_SOLO',
                  'solar_radiation':'Fsd',
                  'temperature':'Ta',
                  'wind_speed': 'Ws_CSAT',
@@ -54,9 +51,10 @@ def get_data(configs_dict):
                'temperature':'Ta_QCFlag',
                'wind_speed': 'Ws_CSAT_QCFlag',
                'friction_velocity': 'ustar_QCFlag'}
-    newNames_dict = {'carbon_flux':'NEE',
-                     'solar_radiation':'PAR',
-                     'temperature':'TempC',
+    newNames_dict = {'carbon_flux':'Fc',
+                     'modelled_carbon_flux': 'Fc_model',
+                     'solar_radiation':'Fsd',
+                     'temperature':'Ta',
                      'wind_speed': 'ws',
                      'friction_velocity': 'ustar'}
 
@@ -80,12 +78,14 @@ def get_data(configs_dict):
                      configs_dict['ustar_threshold'])
     d[vars_dict['carbon_flux']][index] = np.nan
 
-    # Replace configured error values with NaNs and remove data with unacceptable QC codes, 
-    # then drop QC flag variables and rename variable to new names
+    # Replace configured error values with NaNs and remove data with unacceptable 
+    # QC codes, then drop QC flag variables and rename variable to new names
     for key in vars_dict.keys():
-        d[vars_dict[key]] = np.where(d[QC_dict[key]] != configs_dict['QC_accept_code'],
-                                     np.nan, d[vars_dict[key]])
-        d.pop(QC_dict[key])
+        if key in QC_dict.keys():
+            d[vars_dict[key]] = np.where(d[QC_dict[key]] != 
+                                             configs_dict['QC_accept_code'],
+                                         np.nan, d[vars_dict[key]])
+            d.pop(QC_dict[key])
         d[newNames_dict[key]] = d.pop(vars_dict[key])
 
     # Add date_time variable
@@ -95,15 +95,24 @@ def get_data(configs_dict):
 
 def main():    
 
+    results_out_path = '/home/imchugh/Documents'
+
+    # Update
+    reload(r_e)
+
     # Get configurations and data
     configs_dict = get_configs()
     data_dict = get_data(configs_dict)
+
+    # Calculate sigma_delta as a function of flux magnitude
+    fig, stats_dict = r_e.calculate_sigma_delta(data_dict, configs_dict)    
+    fig.savefig(os.path.join(results_out_path, 'Random_error_plots.jpg'))    
+    sig_del = r_e.calculate_sigma_delta(data_dict['Fc'], stats_dict)
     
-    return data_dict
-#    # Return parameter and series dictionaries
-#    param_dict, series_dict = pt.main(data_dict, configs_dict)
-#
-#    return param_dict, series_dict
+    return sig_del
+    
+    
+
 
 
 
