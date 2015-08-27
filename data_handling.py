9ef276613cde5f735ced6e48f3c9db0a4ce9a866
imchugh <ian.mchugh@monash.edu>
2015-08-27 17:07:24 +1000

    Mostly just added doc_strings

--- data_handling.py
+++ data_handling.py
@@ -14,7 +14,16 @@ def subset_data_dict(data_dict, boolean_index, copy = True):
     return {var: data_dict[var][boolean_index] for var in work_dict.keys()}
 
 def subset_datayear_from_arraydict(data_dict, date_time_var, year = None):
-    
+    """
+    Pass: 1) data_dict - a dictionary containing arrays, one of which must be a 
+             python datetime;
+          2) date_time_var - namestring of datetime variable
+          3) year to be returned
+    Returns: if year is specified, return the same dictionary structure with 
+             only data for that year; if no year is specified, it returns a 
+             dictionary with each data year contained as the value with the 
+             year as the key
+    """    
     years_array = np.array([date_.year for date_ in data_dict[date_time_var]])
     if not year:
         year_list = set(list(years_array))    
@@ -29,21 +38,42 @@ def subset_datayear_from_arraydict(data_dict, date_time_var, year = None):
     return new_dict
     
 def subset_onthreshold_from_arraydict(data_dict, ind_var, threshold):
-    
+    """
+    Pass a dictionary
+    """
     hi_index = data_dict[ind_var] > threshold
     lo_index = data_dict[ind_var] < threshold
     return {'>': subset_data_dict(data_dict, hi_index),
             '<': subset_data_dict(data_dict, lo_index)}
+
+def sort_dict_on_index_variable(data_dict, sort_var):
+    """
+    Sorts a dictionary of equal-length numpy arrays on the basis of a
+    sorting variable
+    Pass: 1) data_dict - a dictionary containing arrays
+          2) sort_var - the variable whose sorted order will dictate the 
+                        ordering of all others
+    """
+    index = sort_var.argsort()
+    for key in data_dict.keys():
+        data_dict[key] = data_dict[key][index]
+    return data_dict
             
-def set_arraydict_to_nan_conditional(data_dict, ind_var, threshold, hilo = '>'):
-    
-    if hilo == '>':
-        nan_index = data_dict[ind_var] < threshold
-    elif hilo == '<':
-        nan_index = data_dict[ind_var] > threshold
+def set_arraydict_to_nan_conditional(data_dict, ind_var, value, 
+                                     cond = 'less', set_vars = None):
+    if cond == 'less':
+        nan_index = data_dict[ind_var] < value
+    elif cond == 'greater':
+        nan_index = data_dict[ind_var] > value
+    elif cond == 'equal':
+        nan_index = data_dict[ind_var] == value
     else: return
+    if set_vars:
+        if not isinstance(set_vars, list): set_vars = [set_vars]
+    else:
+        set_vars = data_dict.keys()
     new_dict = data_dict.copy()
-    for var in new_dict.keys():
-        new_dict[var][nan_index] == np.nan
+    for var in set_vars:
+        new_dict[var][nan_index] = np.nan
     return new_dict
     
