7595aacbc24b07b954e3a69499991da9f444c214
imchugh <ian.mchugh@monash.edu>
2015-09-03 17:00:35 +1000

    Not sure

--- uncertainty_wrapper_OzFluxQC.py
+++ uncertainty_wrapper_OzFluxQC.py
@@ -20,7 +20,6 @@ def get_data(configs_dict):
 
     # Initialise name change dictionary with new names via common keys
     vars_dict = configs_dict['variables']
-#    vars_dict['date_and_time_stamp'] = 'date_time'
     newNames_dict = {'carbon_flux':'Fc',
                      'modelled_carbon_flux': 'Fc_model',
                      'solar_radiation':'Fsd',
@@ -71,101 +70,93 @@ def main():
     # Unpack configs and open data file
     configs_master_dict = io.config_to_dict(io.file_select_dialog())
     data_dict, attr = get_data(configs_master_dict)
-    global_configs_dict = configs_master_dict['global_options']
+    global_configs_dict = configs_master_dict['global_configs']
     global_configs_dict['measurement_interval'] = int(attr['time_step'])
     random_configs_dict = dict(global_configs_dict,
-                               **configs_master_dict['random_error_options'])
+                               **configs_master_dict['random_error_configs'])
     model_configs_dict = dict(global_configs_dict,
-                              **configs_master_dict['model_error_options'])
-
-    # Create copy dataset separated into years
-    years_data_dict = data.subset_datayear_from_arraydict(data_dict.copy(), 
-                                                          'date_time')
-    
-    # Create a second copy dataset that is further subdivided into night and day
-    night_day_years_data_dict = {}
-    for year in years_data_dict:
-        subset_dict = (data.subset_onthreshold_from_arraydict
-                           (years_data_dict[year],
-                           'Fsd',
-                           model_configs_dict['noct_threshold']))
-        subset_dict = {'day': subset_dict['>'], 'night': subset_dict['<']}
-        subset_dict['night'] = (data.set_arraydict_to_nan_conditional
-                                    (subset_dict['night'], 
-                                    'ustar', 
-                                     model_configs_dict['ustar_threshold'][year], 
-                                     set_vars = ['Fc']))
-        night_day_years_data_dict[year] = subset_dict
-
-    ################
-    # RANDOM ERROR #
-    ################
+                              **configs_master_dict['model_error_configs'])
 
     # Calculate the linear regression parameters of sigma_delta as a function 
     # of flux magnitude
     fig, stats_dict = rand_err.regress_sigma_delta(data_dict, random_configs_dict)
     fig.savefig(os.path.join(global_configs_dict['output_path'], 
-                             'Random_error_plots.jpg'))
-    
+                             'Random_error_plots.jpg')) 
+                             
     # Calculate estimated sigma_delta for each data point, and remove records 
     # where no observational estimate is available (only has an effect if the 
-    # propagation series is a model - which is recommended!!!); then compound
-    # random error estimates for all records where obs are available, 
-    # then sum and scale appropriately to estimate annual error in gC m-2
+    # propagation series is a model - which is recommended!!!);                             
+    sig_del_array = (rand_err.estimate_sigma_delta
+                        (data_dict[random_configs_dict['propagation_series']], 
+                         stats_dict))
+    sig_del_array[np.isnan(data_dict['Fc'])] = np.nan
+    data_dict['sigma_delta'] = sig_del_array 
 
-    random_error_dict = {}
-    prop_series = random_configs_dict['propagation_series']
+    # Create copy dataset separated into years, screen out low ustar and 
+    # separate into day and night, ready for uncertainty calculations
+    years_data_dict = data.subset_datayear_from_arraydict(data_dict.copy(), 
+                                                          'date_time')                                                          
     for year in years_data_dict.keys():
-        sig_del_array = (rand_err.estimate_sigma_delta
-                            (years_data_dict[year][prop_series], 
-                             stats_dict))
-        sig_del_array = sig_del_array[~np.isnan(years_data_dict[year]['Fc'])]
-        random_error_dict[year] = (rand_err.propagate_random_error
-                                      (sig_del_array,
-                                       random_configs_dict))
-                                    
-
-    ###############
-    # MODEL ERROR #
-    ###############
-
-    # Subset data and calculate model error for each year
-    model_error_dict = {'day': {}, 'night': {}}
-    for year in years_data_dict.keys():
+        ustar_threshold = global_configs_dict['ustar_threshold'][str(year)]
+        noct_threshold = global_configs_dict['noct_threshold']
+        years_data_dict[year]['Fc'][(years_data_dict[year]['Fsd'] < 
+                                     noct_threshold) & 
+                                    (years_data_dict[year]['ustar'] < 
+                                     ustar_threshold)] = np.nan
         subset_dict = (data.subset_onthreshold_from_arraydict(
                            years_data_dict[year],
                            'Fsd',
-                           model_configs_dict['noct_threshold']))
+                           global_configs_dict['noct_threshold']))
         subset_dict = {'day': subset_dict['>'], 'night': subset_dict['<']}
-        subset_dict['night'] = (data.set_arraydict_to_nan_conditional(
-                                    subset_dict['night'], 'ustar', 0.42, 
-                                    set_vars = ['Fc']))
-        for cond in subset_dict:
-            print 'For ' + str(year) + ' ' + cond
-            model_error_dict[cond][year] = (mod_err.model_error
-                                                (subset_dict[cond], 
-                                                 model_configs_dict))    
+        years_data_dict[year] = subset_dict
 
-
-
-    ######################
-    # PARTITIONING ERROR #
-    ######################
-
-#    test_0, test_1 = mod_err.nocturnal_model_error(data_dict, model_configs_dict)
-    
-#    return test_1 
-
-    # Sum all error
-    results_dict = {}
-    results_dict['random_error'] = random_error_dict
-    results_dict['daytime_model_error'] = model_error_dict['day']
-    results_dict['nocturnal_model_error'] = model_error_dict['night']
-    results_dict['combined_error'] = {}
+    # Calculate errors
+    error_dict = {}        
     for year in years_data_dict.keys():
-        results_dict['combined_error'][year] = np.sqrt(
-             (results_dict['random_error'][year])**2 +
-             (results_dict['daytime_model_error'][year])**2 + 
-             (results_dict['nocturnal_model_error'][year])**2)
+        error_dict[year] = {}
+        total_obs = 0
+        for cond in years_data_dict[year].keys():
+            
+            # Grab data            
+            temp_dict = years_data_dict[year][cond]
+            
+            # Do availability stats
+            stats_dict = data.count_nans_in_array(temp_dict['Fc'])
+            total_obs = total_obs + stats_dict['Total_obs']
+            error_dict[year]['Avail_obs_' + cond] = stats_dict['Avail_obs']
+            error_dict[year]['Pct_avail_obs_' + cond] = stats_dict['Pct_avail_obs']
+            
+            # Do random error            
+            error_dict[year]['random_error_' + cond] = (
+                rand_err.propagate_random_error
+                    (temp_dict['sigma_delta'][~np.isnan(temp_dict['sigma_delta'])],
+                     random_configs_dict))
 
-    return results_dict    
+            # Do model error
+            error_dict[year]['model_error_' + cond] = (
+                mod_err.model_error(temp_dict, model_configs_dict))  
+
+        # Do combined stats
+        error_dict[year]['Year'] = year
+        error_dict[year]['Avail_obs_total'] = (
+            error_dict[year]['Avail_obs_day'] +
+            error_dict[year]['Avail_obs_night'])
+        error_dict[year]['Pct_avail_obs_total'] = (
+            round(error_dict[year]['Avail_obs_total'] / float(total_obs) * 100, 1))
+        error_dict[year]['combined_error'] = np.round(np.sqrt(
+            error_dict[year]['model_error_day']**2 +
+            error_dict[year]['model_error_night']**2 +
+            error_dict[year]['random_error_day']**2 +
+            error_dict[year]['random_error_night']**2), 2)
+
+    # Ouput
+    io.dict_to_csv(error_dict, 
+                   ['Year', 'Avail_obs_day', 'Avail_obs_night', 
+                    'Avail_obs_total', 'Pct_avail_obs_day', 
+                    'Pct_avail_obs_night', 'Pct_avail_obs_total', 
+                    'model_error_day', 'model_error_night', 'random_error_day', 
+                    'random_error_night', 'combined_error'],
+                   os.path.join(global_configs_dict['output_path'], 
+                                'Uncertainty_output.csv'))
+    
+    return error_dict
