0fa80b41eb1da2f920b27b1518ffe150a58496b0
imchugh <ian.mchugh@monash.edu>
2016-04-01 16:16:31 +1100

    Changed model error so that can do single runs rather than multiple

--- model_error.py
+++ model_error.py
@@ -6,11 +6,9 @@ Created on Mon Aug 17 14:37:38 2015
 """
 import pdb
 import numpy as np
-import sys
-#sys.path.append('../Partitioning')
-#import Partition_NEE_5 as pt
+from scipy import stats
 
-def model_error(data_dict, configs_dict):
+def estimate_model_error(data_dict, configs_dict):
 
     # Create data arrays, then count all
     obs_array = data_dict['Fc']
@@ -33,19 +31,27 @@ def model_error(data_dict, configs_dict):
     # Get the amount of data that will be removed from the sample (based on the 
     # proportion missing from the complete dataset)
     sample_missing = 1000 - int(1000 * (avail_records / float(total_records)))
+    
     # Draw a random sample of 1000 data from the timeseries, then calculate the
     # difference between the observed and model-spliced series (appropriately 
     # scaled to gC m-2)
-    error_array = np.empty(configs_dict['num_trials'])
+    random_index = np.random.randint(0, len(obs_array), 1000)
+    subset_obs_array = obs_array[random_index]
+    subset_mod_array = mod_array[random_index]
+    subset_splice_array = np.concatenate([subset_mod_array[:sample_missing],
+                                          subset_obs_array[sample_missing:]])
+    obs_sum = subset_obs_array.sum()
+    splice_sum = subset_splice_array.sum()
+    return (obs_sum - splice_sum) / obs_sum * annual_sum
+
+def propagate_model_error(data_dict, configs_dict):
+
+    # Calculate critical t-statistic for p = 0.095
+    crit_t = stats.t.isf(0.025, configs_dict['num_trials'])
+
+    # Create arrayn to hold results then iterate over num_trials
+    error_array = np.empty(configs_dict['num_trials'])        
     for this_trial in xrange(configs_dict['num_trials']):
-        random_index = np.random.randint(0, len(obs_array), 1000)
-        subset_obs_array = obs_array[random_index]
-        subset_mod_array = mod_array[random_index]
-        subset_splice_array = np.concatenate([subset_mod_array[:sample_missing],
-                                              subset_obs_array[sample_missing:]])
-        obs_sum = subset_obs_array.sum()
-        splice_sum = subset_splice_array.sum()
-        error_array[this_trial] = (obs_sum - splice_sum) / obs_sum * annual_sum
-    total_error = error_array.std() * 2
-    
-    return np.round(total_error, 2)
+        error_array[this_trial] = estimate_model_error(data_dict, configs_dict)
+        
+    return np.round(error_array.std() * crit_t, 2)    
