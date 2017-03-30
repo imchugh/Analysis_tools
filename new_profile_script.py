d3eaec59a88f215e7a58eb2bf281de5439da5922
Ian McHugh <ian_mchugh@fastmail.com>
2017-03-30 17:19:47 +1100

    Class for profile processing

--- new_profile_script.py
+++ new_profile_script.py
@@ -14,12 +14,14 @@ import matplotlib.pyplot as plt
 
 class storage(object):
     
-    def __init__(self, target_file):
+    def __init__(self, target_file, CO2_str = 'CO2', Ta_str = 'Tair'):
         
         self.target_file = target_file
         self.df = self.get_data()
-        self.CO2_channels = [var for var in self.df.columns if 'CO2' in var]
-        self.T_channels = [var for var in self.df.columns if 'Tair' in var]
+        self.CO2_level_names, self.CO2_levels = self.get_level_names(CO2_str)
+        self.T_channels = [var for var in self.df.columns if Ta_str in var]
+#        self.levels = [int(this_label.split('_')[1][:-1]) for 
+#                       this_label in self.CO2_channels]
 
     def get_data(self):
     
@@ -35,19 +37,46 @@ class storage(object):
                   'smooth_window' (int, minutes) - applies a centered running 
                   average over the requested interval
         """
-        pdb.set_trace()
-        self.df.index = pd.to_datetime(df.Datetime)
-        self.df.drop('Datetime', axis = 1, inplace = True)
+        
+        if not isinstance(output_freq, int):
+            raise Exception('Output frequency must be an integer (units = minutes)')
+        output_freq_string = '{0}T'.format(str(output_freq))
+
+        local_df = cp.copy(self.df)
+
+        local_df.index = pd.to_datetime(local_df.Datetime)
+        local_df.drop('Datetime', axis = 1, inplace = True)
         
         if not smooth_window == 0:
-            current_freq = pd.infer_freq(self.df.index)
-            upsample_df = self.df.resample('1T').interpolate()
+            current_freq = pd.infer_freq(local_df.index)
+            upsample_df = local_df.resample('1T').interpolate()
             smooth_df = upsample_df.rolling(window = smooth_window, 
                                             center = True).mean()
             downsample_df = smooth_df.resample(current_freq).pad()
-            return downsample_df.resample('30T').pad()
+            return downsample_df.resample(output_freq_string).pad()
         else:
-            return downsample_df.resample('30T').pad()
+            return local_df.resample(output_freq_string).pad()
+
+    def get_level_names(self, search_str, return_levels = True):
+
+        name_list = [var for var in self.df.columns if search_str in var]        
+        
+        str_levels_list = [name.split('_')[1][:-1] for name in name_list]
+        num_levels_list = []
+        for level in str_levels_list:
+            try:
+                num_levels_list.append(int(level))
+            except ValueError:
+                num_levels_list.append(float(level))
+        sorted_levels_list = [i[1] for i in sorted(enumerate(num_levels_list), 
+                              key = lambda x: x[1])]
+        sort_idx = [i[0] for i in sorted(enumerate(num_levels_list), 
+                    key = lambda x: x[1])]
+        sorted_name_list = [name_list[i] for i in sort_idx]
+        if return_levels:
+            return sorted_name_list, sorted_levels_list
+        else:
+            return sorted_name_list
 
 def calculate_CO2_storage(df, site_alt = None):
     
@@ -89,28 +118,6 @@ def get_standard_press(elevation):
     
     return 101.3
 
-def sort_level_names(name_list, return_levels = True):
-    
-    str_levels_list = [name.split('_')[1][:-1] for name in name_list]
-    num_levels_list = []
-    for level in str_levels_list:
-        try:
-            num_levels_list.append(int(level))
-        except ValueError:
-            num_levels_list.append(float(level))
-    sorted_levels_list = [i[1] for i in sorted(enumerate(num_levels_list), 
-                          key = lambda x: x[1])]
-    sort_idx = [i[0] for i in sorted(enumerate(num_levels_list), 
-                key = lambda x: x[1])]
-    sorted_name_list = [name_list[i] for i in sort_idx]
-    if return_levels:
-        return sorted_name_list, sorted_levels_list
-    else:
-        return sorted_name_list
-
-def get_names(df, var = 'CO2'):
-    
-    return [this_var for this_var in df.columns if var in this_var]
 
 def get_layer_names(levels_list, var = 'CO2'):
     
@@ -165,55 +172,55 @@ output_int = 30
 smooth_window = 10
 use_Tair_var = False
 
-this_df = pd.read_csv(path_to_file)
-df = downsample_data(this_df, smooth_window = 10)
-
-if not 'press' in df.columns:
-    if not site_alt is None:
-        pass
-    else:
-        df['press'] = default_press
-
-# Make name and level lists for CO2
-CO2_level_names_list = [var for var in df.columns if 'CO2' in var]
-CO2_level_names_list, CO2_levels_list = sort_level_names(CO2_level_names_list)
-
-# Make name and level lists for T (include various checks)
-Tair_level_names_list = [var for var in df.columns if 'Tair' in var]
-if use_Tair_var:
-    if not use_Tair_var in Tair_level_names_list:
-        raise KeyError('User-specified air temperature variable not found in '
-                        'data file - exiting!')
-    n_Tair = 1
-else:
-    if len(Tair_level_names_list) == 1:
-        use_Tair_var == Tair_level_names_list[0]
-        n_Tair = 1
-    if len(Tair_level_names_list) == len(CO2_level_names_list):
-        Tair_level_names_list, Tair_levels_list = sort_level_names(Tair_level_names_list)
-        n_Tair = len(CO2_level_names_list)
-    else:
-        raise Exception('Number of temperature variables does not match the '
-                        'number of CO2 variables - when this is the case, '
-                        'either a single temperature variable should be '
-                        'included or the name of the desired temperature '
-                        'variable should be passed as a keyword argument '
-                        '(use_Ta_var); exiting!') 
-
-CO2_layers_df = get_layer_means(df)
-if n_Tair == 1:
-    layers_df = CO2_layers_df
-    layers_df['Tair'] = df[use_Tair_var]
-else:    
-    Tair_layers_df = get_layer_means(df, var = 'Tair')
-    layers_df = CO2_layers_df.join(Tair_layers_df)
-
-CO2_layer_names_list = get_layer_names(CO2_levels_list)
-
-test = calculate_CO2_storage(layers_df)
-
-diurnal_df = test.groupby([lambda x: x.hour, lambda y: y.minute]).mean()
-diurnal_df.index = np.arange(48) / 2.0
+#this_df = pd.read_csv(path_to_file)
+#df = downsample_data(this_df, smooth_window = 10)
+#
+#if not 'press' in df.columns:
+#    if not site_alt is None:
+#        pass
+#    else:
+#        df['press'] = default_press
+#
+## Make name and level lists for CO2
+#CO2_level_names_list = [var for var in df.columns if 'CO2' in var]
+#CO2_level_names_list, CO2_levels_list = sort_level_names(CO2_level_names_list)
+#
+## Make name and level lists for T (include various checks)
+#Tair_level_names_list = [var for var in df.columns if 'Tair' in var]
+#if use_Tair_var:
+#    if not use_Tair_var in Tair_level_names_list:
+#        raise KeyError('User-specified air temperature variable not found in '
+#                        'data file - exiting!')
+#    n_Tair = 1
+#else:
+#    if len(Tair_level_names_list) == 1:
+#        use_Tair_var == Tair_level_names_list[0]
+#        n_Tair = 1
+#    if len(Tair_level_names_list) == len(CO2_level_names_list):
+#        Tair_level_names_list, Tair_levels_list = sort_level_names(Tair_level_names_list)
+#        n_Tair = len(CO2_level_names_list)
+#    else:
+#        raise Exception('Number of temperature variables does not match the '
+#                        'number of CO2 variables - when this is the case, '
+#                        'either a single temperature variable should be '
+#                        'included or the name of the desired temperature '
+#                        'variable should be passed as a keyword argument '
+#                        '(use_Ta_var); exiting!') 
+#
+#CO2_layers_df = get_layer_means(df)
+#if n_Tair == 1:
+#    layers_df = CO2_layers_df
+#    layers_df['Tair'] = df[use_Tair_var]
+#else:    
+#    Tair_layers_df = get_layer_means(df, var = 'Tair')
+#    layers_df = CO2_layers_df.join(Tair_layers_df)
+#
+#CO2_layer_names_list = get_layer_names(CO2_levels_list)
+#
+#test = calculate_CO2_storage(layers_df)
+#
+#diurnal_df = test.groupby([lambda x: x.hour, lambda y: y.minute]).mean()
+#diurnal_df.index = np.arange(48) / 2.0
 
 
 #fig, ax = plt.subplots(1, 1, figsize = (12, 8))
