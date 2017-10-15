13f8691e4d325a95c8f879fba42c93c2632bd5e4
Ian McHugh <ian_mchugh@fastmail.com>
2017-10-16 10:20:17 +1100

    New version that uses lmfit

---
+++ respiration_new.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 17:07:47 2017
+
+@author: ian
+"""
+
+import calendar
+import datetime as dt
+from lmfit import Model
+import numpy as np
+import pandas as pd
+import sys
+import pdb
+
+import DataIO as io
+
+###############################################################################
+# Functions                                                                   #
+###############################################################################
+
+#------------------------------------------------------------------------------
+def check_slice_length(sub_df, configs_dict, year = None):
+    
+    interval_mins = int(filter(str.isdigit, configs_dict['interval']))
+    if year is None:
+        pct_required = configs_dict['min_data_pct_window']
+        expected_length = 1440 / interval_mins * configs_dict['window_size']
+    else:
+        n_days = 366 if calendar.isleap(year) else 365
+        expected_length = 1440 / interval_mins * n_days
+        pct_required = configs_dict['min_data_pct_annual']
+    pct_available = len(sub_df) / float(expected_length) * 100
+    assert pct_available > pct_required
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+def get_data(configs_dict):
+
+    df = io.OzFluxQCnc_to_data_structure(configs_dict['file_path'], 
+                                         output_structure='pandas') 
+    try:
+        freq = pd.infer_freq(df.index)
+        assert freq == '30T' or '60T'
+    except AssertionError:
+        print 'File is not chronologically continuous, exiting...'
+        sys.exit()
+    configs_dict['interval'] = freq
+    return df
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+def make_date_iterator(df, configs_dict):
+    
+    size = configs_dict['window_size']
+    step = configs_dict['window_step']
+    start_date = df.index[0].to_pydatetime().date() + dt.timedelta(size / 2)
+    end_date = df.index[-1].to_pydatetime().date()
+    freq_str = '{}D'.format(str(int(step)))
+    dates = (pd.date_range(start_date, end_date, freq = freq_str) + 
+             dt.timedelta(0.5))
+    date_bounds = zip(dates - dt.timedelta(size / 2.0 - 30 / 1440.0),
+                      dates + dt.timedelta(size / 2.0))
+    return dict(zip(dates, date_bounds))
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+def optimise(df, model, params):
+    result = model.fit(df.Fc_EP, t_series = df.Ts, vwc_series = df.Sws, 
+                       params = params)
+#    if params['Eo'] < 50 or params['Eo'] > 400:
+        
+    return result
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+def process_data(df, configs_dict):
+
+    # Generate a date iterator based on window size and step
+    date_iterator_dict = make_date_iterator(df, configs_dict)
+    
+    # Make a result dataframe to hold parameter values
+    dates_list = sorted(date_iterator_dict.keys())
+    params_df = pd.DataFrame(index = dates_list, 
+                             columns = ['rb', 'Eo', 'theta_1', 'theta_2', 'QCFlag'])
+    
+    # Initialise the model
+    model = Model(response_func, independent_vars = ['t_series', 
+                                                     'vwc_series'])
+    params = model.make_params(rb = 1, Eo = 200, theta_1 = 1, theta_2 = 10)
+    
+    # Iterate on year
+    years_list = sorted(list(set(df.index.year)))
+    for this_year in years_list:
+        sub_df = (df.loc[df.Fsd < 5]
+                  .loc[str(this_year), ['Fc_EP', 'Ts', 'Sws']].dropna())
+        try:
+            check_slice_length(sub_df, configs_dict, year = this_year)
+        except AssertionError:
+            params_df.loc[str(this_year), 'QCFlag'] = 1
+            continue
+        result = optimise(sub_df, model, params)
+        for param in result.best_values.keys():
+            if not param == 'rb':
+                params_df.loc[str(this_year), param] = result.best_values[param]
+   
+    # Fix parameters
+    params['Eo'].vary = False
+    params['theta_1'].vary = False
+    params['theta_2'].vary = False
+    
+    # Iterate on date
+    for this_date in dates_list:
+        start_date = date_iterator_dict[this_date][0]
+        end_date = date_iterator_dict[this_date][1]
+        sub_df = (df.loc[df.Fsd < 5]
+                  .loc[start_date: end_date, ['Fc_EP', 'Ts', 'Sws']].dropna())
+        try:
+            check_slice_length(sub_df, configs_dict)
+        except AssertionError:
+            params_df.loc[str(this_year), 'QCFlag'] = 2
+            continue
+        result = model.fit(sub_df.Fc_EP, t_series = sub_df.Ts, 
+                           vwc_series = df.Sws, params = params)
+        params_df.loc[this_date, 'rb'] = result.best_values['rb']
+    
+    return params_df
+#------------------------------------------------------------------------------    
+
+#------------------------------------------------------------------------------
+def response_func(t_series, vwc_series, rb, Eo, theta_1, theta_2):
+    
+    return (rb  * np.exp(Eo * (1 / (10 + 46.02) - 1 / (t_series + 46.02))) *
+            1 / (1 + np.exp(theta_1 - theta_2 * vwc_series)))
+#------------------------------------------------------------------------------
+    
+###############################################################################
+# Main program                                                                #
+###############################################################################
+    
+# Set window size and step (both in units of days)
+configs_dict = {'file_path': ('/home/ian/OzFlux/Sites/GatumPasture/Data/'
+                              'Processed/All/GatumPasture_L3.nc'),
+                'window_size': 10, 
+                'window_step': 5,
+                'min_data_pct_window': 20,
+                'min_data_pct_annual': 10}
+
+# Get the data and check timestamp integrity (exit if not continuous)
+df = get_data(configs_dict)
+
+# Get parameters
+params_df = process_data(df, configs_dict)
+
+#sub_df = df.loc[start_date: end_date, ['Fc', 'Ts', 'Sws']]
