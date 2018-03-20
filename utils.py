ceb596759522efecc72fae9515209889cd2d6d0c
Ian McHugh <ian_mchugh@fastmail.com>
2018-03-20 13:06:05 +1100

    Utility script for a bunch of common functions

--- utils.py
+++ utils.py
@@ -12,4 +12,9 @@ import pdb
 def rename_df(df, external_names, internal_names):
     
     assert all(sorted(external_names.keys()) == sorted(internal_names.keys))
+    swap_dict = {external_names[key]: internal_names[key] 
+                 for key in internal_names.keys()}
+    sub_df = df[swap_dict.keys()].copy()
+    sub_df.columns = swap_dict.values()
+    return sub_df
     pdb.set_trace()
